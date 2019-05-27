f781d8cd9f8aa6002d88add9b7ac856f6072577b
Kim Drezdon <kdrezdon@gmail.com>
2019-05-27 11:24:39 -0500

    Add comments

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
+//Require express
 const express = require('express');
-
 const app = express();
 
+//Require JSON data object
 const data = require('./data/data.json');
 const projects = data.projects
 
+//Serve static files
 app.use('/static', express.static('public'));
 
+//Set Pug as the templating engine
 app.set('view engine', 'pug');
 
+//Route for Index page
 app.get('/', (req, res) => {
     const templateData = { projects };
-
+    
     res.render('index', templateData);
 })
 
+//Route for About page
 app.get('/about', (req, res) => {
     res.render('about');
 })
 
+//Route for project pages
 app.get('/projects/:id', (req, res, next) => {
     const { id } = req.params;
     if (id > 4 || isNaN(id)) {
@@ -37,18 +43,21 @@ app.get('/projects/:id', (req, res, next) => {
     res.render('project', templateData);
 });
 
+//Error route
 app.use((req, res, next) => {
     const err = new Error('Looks like you may have gotten lost!');
     err.status = 404;
     next(err);
 })
 
+//Error middleware
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
 })
 
+//Heroku port setup
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
